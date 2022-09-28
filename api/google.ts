23dc6de3c044b2a8ba42ce1fc5c438badbed1e0b
Keith Baker <github@baker.af>
2022-09-28 11:06:54 -0400

    add interceptor for debugging

--- api/google.ts
+++ api/google.ts
@@ -1,11 +1,21 @@
 import axios from "axios";
 import { GOOGLE_API_KEY } from '@env';
+import yelp from "./yelp";
 
 const GOOGLE_MAP_URL: string = `https://maps.googleapis.com/maps/api/geocode/json`
 
-export default axios.create({
+const google = axios.create({
     baseURL: GOOGLE_MAP_URL,
     headers: {
         Authorization: `Bearer ${GOOGLE_API_KEY}`
     }
 })
+
+google.interceptors.request.use(function (config) {
+    console.log(`pre google call`)
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+});
+
+export default google
--- api/yelp.ts
+++ api/yelp.ts
@@ -1,11 +1,20 @@
-import axios from 'axios'
-import { YELP_API_KEY } from '@env';
+import axios from "axios";
+import { YELP_API_KEY } from "@env";
 
-const YELP_URL = `https://api.yelp.com/v3/businesses`
+const YELP_URL = `https://api.yelp.com/v3/businesses`;
 
-export default axios.create({
-  baseURL: YELP_URL,
-  headers: {
-    Authorization: `Bearer ${ YELP_API_KEY }`
-  }
-})
+const yelp = axios.create({
+	baseURL: YELP_URL,
+	headers: {
+		Authorization: `Bearer ${YELP_API_KEY}`,
+	},
+});
+
+yelp.interceptors.request.use(function (config) {
+	console.log(`pre yelp call`)
+	return config;
+}, function (error) {
+	return Promise.reject(error);
+});
+
+export default yelp;
