7f3758b335e88596629a56f4ece016ae1266eb14
Keith Baker <github@baker.af>
2022-11-13 15:22:10 -0500

    updated types

--- types.tsx
+++ types.tsx
@@ -16,7 +16,7 @@ declare global {
 
 export type RootStackParamList = {
 	Root: NavigatorScreenParams<RootTabParamList> | undefined;
-	Modal: undefined;
+	Modal: NavigatorScreenParams<RootTabParamList> | undefined;
 	NotFound: undefined;
 };
 
