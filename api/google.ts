37a102f823b42e25102575cf1b9b5d11476fb885
Keith Baker <github@baker.af>
2022-11-13 15:42:22 -0500

    updated google api

--- api/google.ts
+++ api/google.ts
@@ -1,20 +1,19 @@
 import axios from "axios";
-import { GOOGLE_API_KEY } from '@env';
-import yelp from "./yelp";
+import { GOOGLE_API_KEY } from "@env";
 
-const GOOGLE_MAP_URL: string = `https://maps.googleapis.com/maps/api/geocode/json`
+const GOOGLE_MAP_URL: string = `https://maps.googleapis.com/maps/api/geocode/json`;
 
 const google = axios.create({
-    baseURL: GOOGLE_MAP_URL,
-    headers: {
-        Authorization: `Bearer ${GOOGLE_API_KEY}`
-    }
-})
-
-google.interceptors.request.use(function (config) {
-    return config;
-}, function (error) {
-    return Promise.reject(error);
+	baseURL: GOOGLE_MAP_URL,
+	headers: {
+		Authorization: `Bearer ${GOOGLE_API_KEY}`,
+	},
 });
 
-export default google
+google.interceptors.request.use(function (config) {
+	return config;
+}, function (error) {
+	return Promise.reject(error);
+});
+
+export default google;
